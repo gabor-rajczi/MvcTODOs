2662a54b026693c7e4616fb43addc77e32bf82e8
rajczig <rajczi@dexter.hu>
2016-04-05 11:06:55 +0200

    JS-ek átpakolása másik "névtérbe"

--- MvcToDos/Scripts/Sajat/Home.js
+++ MvcToDos/Scripts/Sajat/Home.js
@@ -1,15 +1,31 @@
-﻿var Home = function () {
-    var onCreateSuccess = function () {
-        $(".Create form")[0].reset();
-        $("#Szoveg").focus();
-    }
-    var onEditSucces = function (id) {
+﻿var Edit = function () {
+    var onSucces = function (id) {
         $(id).toggleClass("Kesz");
     }
     return {
-        onCreateSuccess: onCreateSuccess,
-        onEditSucces: onEditSucces
+        onSucces: onSucces
     };
 }();
 
-
+var Create = function() {
+    var onInit = function() {
+        $(document).ready(function() {
+            $("#Szoveg").focus();
+            $("#Szoveg").keypress(function(e) {
+                if (e.which == 13) {
+                    e.preventDefault();
+                    $(".Create input[type='submit']").focus().click();
+                    return false;
+                }
+            });
+        });
+    };
+    var onSuccess = function() {
+        $(".Create form")[0].reset();
+        $("#Szoveg").focus();
+    };
+    return {
+        onInit: onInit,
+        onSuccess: onSuccess
+    };
+}();
